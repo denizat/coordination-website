a694b4ddc426819a981ee7120443759bc538b011
denizat <deniz@telci.org>
2022-06-14 08:23:09 -0500

    lots of progress

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React, { MouseEventHandler, ReactElement } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// interface Person {
-//   name: string
-//   color: string
-//   busyTime: boolean[]
-// }
+// https://random.responsiveimages.io/
+// https://tailwindcss.com/docs/customizing-colors
+
 class Person {
   name: string;
   color: string;
@@ -17,8 +15,23 @@ class Person {
   }
 }
 
+interface ButtonProps {
+  onClick: MouseEventHandler;
+  text: string;
+}
+const Button: React.FC<ButtonProps> = (props: ButtonProps): ReactElement => {
+  return (
+    <div
+      className="cursor-pointer bg-blue-200 hover:bg-blue-500 text-black rounded-md w-min p-2"
+      onClick={props.onClick}
+    >
+      <pre>{props.text}</pre>
+    </div>
+  );
+};
+
 class PeopleModal extends React.Component<
-  { showModal: boolean; submitter: Function; closeModal: MouseEventHandler },
+  { showModal: boolean; submitter: Function; closeModal: Function },
   {
     nameValue: string;
     colorValue: string;
@@ -48,45 +61,61 @@ class PeopleModal extends React.Component<
   }
 
   render() {
+    let modalName = String(Math.random());
     return this.props.showModal ? (
       <div
-        style={{ top: "5%", left: "5%", width: "90%", height: "90%" }}
-        className="fixed  bg-blue-500"
+        className="fixed top-0 left-0 w-full h-full z-40 bg-opacity-90 bg-black"
+        onClick={(e) => {
+          if (e.target !== document.getElementById(modalName)) {
+            this.props.closeModal(e);
+          }
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Escape") {
+            this.props.closeModal(e);
+          }
+        }}
       >
-        <form>
-          <label>
-            Name:
-            <input
-              autoFocus
-              type="text"
-              name="nameValue"
-              value={this.state.nameValue}
-              onChange={this.handleInputChange}
-            />
-          </label>
-          <label>
-            Color:
-            <input
-              type="text"
-              name="colorValue"
-              value={this.state.colorValue}
-              onChange={this.handleInputChange}
-            />
-          </label>
-          <button
-            onClick={(e) => {
-              this.setState({
-                colorValue: "",
-                nameValue: "",
-              });
-              this.handleSubmit(e);
-              this.props.closeModal(e);
-            }}
-          >
-            Submit
-          </button>
-        </form>
-        <div onClick={this.props.closeModal}>close</div>
+        <div
+          id={modalName}
+          style={{ top: "20%", left: "20%", width: "40%", height: "40%" }}
+          className="fixed  bg-blue-500 z-40 text-black"
+        >
+          <form>
+            <label>
+              Name:
+              <input
+                autoFocus
+                type="text"
+                name="nameValue"
+                value={this.state.nameValue}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <label>
+              Color:
+              <input
+                type="text"
+                name="colorValue"
+                value={this.state.colorValue}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <button
+              onClick={(e) => {
+                this.setState({
+                  colorValue: "",
+                  nameValue: "",
+                });
+                this.handleSubmit(e);
+                this.props.closeModal(e);
+              }}
+            >
+              Submit
+            </button>
+          </form>
+          {/* <Button onClick={this.props.closeModal} text="close" /> */}
+        </div>
       </div>
     ) : null;
   }
@@ -150,12 +179,16 @@ class PeopleHandler extends React.Component<
         ))}
         <div>Current CPI: {this.state.CPI}</div>
         <div>Current Person: {this.state.people[this.state.CPI].name}</div>
-        <button
+        <Button
+          onClick={() => this.setState({ showModal: true })}
+          text="Add Person"
+        />
+        {/* <button
           className="bg-blue-600"
           onClick={() => this.setState({ showModal: true })}
         >
           Add Person
-        </button>
+        </button> */}
         <PeopleModal
           showModal={this.state.showModal}
           submitter={this.addPerson}
@@ -172,11 +205,6 @@ class PeopleHandler extends React.Component<
   }
 }
 
-// class TimeHandler extends React.Component<{
-//   people:Person[],
-//   CPI: number,
-//   personTimeEditor:Function,
-// }
 interface TimeHandlerProps {
   people: Person[];
   // cpi: number;
@@ -185,9 +213,21 @@ interface TimeHandlerProps {
 const TimeHandler: React.FC<TimeHandlerProps> = (
   props: TimeHandlerProps
 ): ReactElement => {
+  // total percent of people busy at a time index
+  const busyAt = (index: number) => {
+    let res =
+      props.people.filter((person) => person.busyTime[index]).length /
+      props.people.length;
+    console.log(res);
+
+    return res * 100;
+  };
   return (
-    <div className="w-3/5">
-      HELLOOOOO
+    <div className="w-1/2 left-1/4 absolute">
+      <div className="border-4 border-purple-400 flex flex-row">
+        <Button onClick={() => {}} text="AM" />
+        <Button onClick={() => {}} text="PM" />
+      </div>
       <div>
         {props.people.map((v, i) => {
           return (
@@ -198,41 +238,56 @@ const TimeHandler: React.FC<TimeHandlerProps> = (
                 width: String(100 / props.people.length) + "%", // there must be a proper way to do this
                 display: "inline-block",
                 borderColor: v.color,
+                backgroundColor: v.color,
               }}
             >
-              {v.name}
+              <div className="bg-gray-800 w-min m-auto rounded-md h-full text-lg p-2">
+                {v.name}
+              </div>
             </div>
           );
         })}
       </div>
-      {Array.from({ length: 24 }).map((v, ai) => {
-        return (
-          <div
-            className="border-4 border-blue-500"
-            key={ai}
-            onClick={() => props.personTimeEditor(ai)}
-          >
-            {/* {i} is person[0] busy now:{" "}
-            {props.people[0].busyTime[i] ? "yes" : "no"} */}
-            {props.people.map((v, i) => {
-              return (
+      <div className="cursor-pointer">
+        {Array.from({ length: 24 }).map((v, ai) => {
+          return (
+            <div
+              className="border-4 border-blue-500"
+              key={ai}
+              onClick={() => props.personTimeEditor(ai)}
+            >
+              {props.people.map((v, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="border-4 h-16"
+                    style={{
+                      width: String(100 / props.people.length) + "%", // there must be a proper way to do this
+                      display: "inline-block",
+                      borderColor: v.color,
+                      backgroundColor: v.busyTime[ai] ? v.color : undefined,
+                    }}
+                  >
+                    {/* is {v.name} busy now?
+                    {v.busyTime[ai] ? "yes" : "no"} */}
+                  </div>
+                );
+              })}
+
+              {
                 <div
-                  key={i}
-                  className="border-4"
+                  className="inline-block select-none"
                   style={{
-                    width: String(100 / props.people.length) + "%", // there must be a proper way to do this
-                    display: "inline-block",
-                    borderColor: v.color,
+                    backgroundColor: `hsl(0,100%,${100 - busyAt(ai) / 2}%`,
                   }}
                 >
-                  is person[{ai}] busy now?
-                  {v.busyTime[ai] ? "yes" : "no"}
+                  busy
                 </div>
-              );
-            })}
-          </div>
-        );
-      })}
+              }
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
